bcd259064b94949160d58f1c374a570030fb8ba3
MoscowDev <mosesogomegbunam@gmail.com>
2026-03-05 21:45:37 +0100

    new commit

--- src/main/java/UnaryOperatorImpl.java
+++ src/main/java/UnaryOperatorImpl.java
@@ -8,5 +8,10 @@ public class UnaryOperatorImpl {
 
         UnaryOperator <Integer> unaryOperator1 = i -> i*i;
        System.out.println(unaryOperator1.apply(2));
+
+
+        UnaryOperator<Integer> unaryOperator2 = (i) -> i*i;
+        Integer a = unaryOperator2.apply(2);
+        System.out.println(a);
     }
 }
